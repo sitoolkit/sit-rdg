40b06e8ee531f54c58c4056340daf973c15d4d6f
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2020-04-12 20:38:16 +0900

    Add detail log

--- sit-rdg-core/src/main/java/io/sitoolkit/rdg/core/application/DependentRowDataGenFunc.java
+++ sit-rdg-core/src/main/java/io/sitoolkit/rdg/core/application/DependentRowDataGenFunc.java
@@ -21,7 +21,12 @@ class DependentRowDataGenFunc implements Function<TableDef, RowData> {
   public RowData apply(TableDef table) {
     RowData rowData = new RowData();
 
+    log.trace("====== Start generating data");
+
     for (RelationDef relation : table.getSortedRelations()) {
+
+      log.trace("Generating relational data for {}", relation);
+
       if (table.equals(relation.getLeftTable())) {
 
         Optional<RelationDef> parentOpt = relation.getParent();
@@ -33,31 +38,28 @@ class DependentRowDataGenFunc implements Function<TableDef, RowData> {
           dataStore.put(relation, newRelatedData);
           rowData.putAll(newRelatedData);
 
-          if (log.isTraceEnabled()) {
-            log.trace("storedData: {}, newRelatedData: {}", storedData, newRelatedData);
-          }
+          log.trace("Get data from store: {}, append data: {}", storedData, newRelatedData);
 
         } else {
+
           RowData relatedData = RowDataGenerator.generate(relation, config);
           dataStore.put(relation, relatedData);
           rowData.putAll(relatedData);
 
-          if (log.isTraceEnabled()) {
-            log.trace("relatedData: {}", relatedData);
-          }
+          log.trace("Generate data: {}", relatedData);
         }
 
       } else {
         RowData storedData = dataStore.get(relation);
         rowData.putAllWithMainToSub(storedData, relation);
 
-        if (log.isTraceEnabled()) {
-          log.trace("storedData: {}", storedData);
-        }
+        log.trace("Get data from stored: {}", storedData);
       }
-      log.trace("rowData: {}", rowData);
     }
+
     RowDataGenerator.fill(rowData, table, config);
+
+    log.trace("Generated data: {}", rowData);
 
     return rowData;
   }
