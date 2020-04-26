e45281f9ff8b657a8f8ff1c35a9c11ededdc7b73
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2020-04-26 19:12:38 +0900

    Simplyfy RelationDef

--- sit-rdg-core/src/main/java/io/sitoolkit/rdg/core/domain/schema/RelationDef.java
+++ sit-rdg-core/src/main/java/io/sitoolkit/rdg/core/domain/schema/RelationDef.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @ToString(onlyExplicitlyIncluded = true, doNotUseGetters = true)
@@ -20,7 +19,7 @@ public class RelationDef {
 
   @Getter @JsonBackReference private SchemaDef schema;
 
-  @JsonManagedReference @EqualsAndHashCode.Include @Getter @Setter @ToString.Include
+  @JsonManagedReference @EqualsAndHashCode.Include @Getter @ToString.Include
   private List<ColumnPair> columnPairs = new ArrayList<>();
 
   @JsonIgnore
@@ -41,59 +40,37 @@ public class RelationDef {
           .distinct()
           .collect(Collectors.toList());
 
+  @Getter(lazy = true)
   @JsonIgnore
-  public List<ColumnDef> getDistinctColumns() {
-    return columnPairs.stream()
-        .map(ColumnPair::getColumns)
-        .flatMap(Collection::stream)
-        .distinct()
-        .collect(Collectors.toList());
-  }
+  private final List<ColumnDef> mainColumns =
+      columnPairs.stream().map(ColumnPair::getMain).collect(Collectors.toList());
 
-  public RelationDef(ColumnPair pair) {
-    columnPairs.add(pair);
-  }
+  @Getter(lazy = true)
+  @JsonIgnore
+  private final List<ColumnDef> subColumns =
+      columnPairs.stream().map(ColumnPair::getSub).collect(Collectors.toList());
+
+  @Getter(lazy = true)
+  @JsonIgnore
+  private final TableDef mainTable = columnPairs.iterator().next().getMain().getTable();
+
+  @Getter(lazy = true)
+  @JsonIgnore
+  private final TableDef subTable = columnPairs.iterator().next().getSub().getTable();
+
+  @Getter(lazy = true)
+  @JsonIgnore
+  private final int size = getMainColumns().size();
+
+  @Getter(lazy = true)
+  @JsonIgnore
+  private final boolean selfRelation = getSubTable().equals(getMainTable());
 
   public void addAllPairs(Collection<ColumnPair> pairs) {
     columnPairs.addAll(pairs);
   }
 
-  public boolean containsAnyInPair(ColumnPair pair) {
-    return getDistinctColumns().parallelStream().anyMatch(col -> pair.getColumns().contains(col));
-  }
-
-  @JsonIgnore
-  public List<ColumnDef> getMainColumns() {
-    return columnPairs.stream().map(ColumnPair::getMain).collect(Collectors.toList());
-  }
-
-  @JsonIgnore
-  public List<ColumnDef> getSubColumns() {
-    return columnPairs.stream().map(ColumnPair::getSub).collect(Collectors.toList());
-  }
-
-  @JsonIgnore
-  public TableDef getMainTable() {
-    return columnPairs.iterator().next().getMain().getTable();
-  }
-
-  @JsonIgnore
-  public TableDef getSubTable() {
-    return columnPairs.iterator().next().getSub().getTable();
-  }
-
-  @JsonIgnore
-  public int getSize() {
-    return getMainColumns().size();
-  }
-
-  @JsonIgnore
-  public boolean isSelfRelation() {
-    return getSubTable().equals(getMainTable());
-  }
-
-  @JsonIgnore
-  public List<UniqueConstraintDef> initMainUniqueConstraints() {
+  private List<UniqueConstraintDef> initMainUniqueConstraints() {
     List<UniqueConstraintDef> mainUniques = new ArrayList<>();
     for (UniqueConstraintDef unique : getMainTable().getUniqueConstraints()) {
       if (unique.getColumns().equals(getMainColumns())) {
