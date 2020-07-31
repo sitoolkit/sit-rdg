10c4bd7e0ab09651633a4327eb8558dc54e8b721
kakeru.k <kakl2612@gmail.com>
2020-07-31 11:31:27 +0900

    Delete unnecessary test case

--- sit-rdg-core/src/test/java/io/sitoolkit/rdg/core/application/DataGeneratorOptimizedImplTest.java
+++ sit-rdg-core/src/test/java/io/sitoolkit/rdg/core/application/DataGeneratorOptimizedImplTest.java
@@ -63,9 +63,4 @@ public class DataGeneratorOptimizedImplTest {
   public void testEmptyDataStore() throws IOException {
     E2eTestHelper.doTest(this, testName.getMethodName(), generator);
   }
-
-  @Test
-  public void testIgnoreScale() throws IOException {
-    E2eTestHelper.doTest(this, testName.getMethodName(), generator);
-  }
 }
